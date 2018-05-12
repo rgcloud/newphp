7db37ae944b96fc497e8c018a24ca2e92189a8a5
rgcloud <37324173+rgcloud@users.noreply.github.com>
2018-05-12 19:15:00 +0530

    v0

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<title> ..:::: Welcome To SGcloud ::::.. </title>
+<center> <h1> Welcome To Our Openshift DevOps(CI/CD) Project</h1> </center>
+
+<?php
+
+echo "<center><h2>Hi Bro How have you been :-)  </h2></center>";
+
+?>
+
+</body>
+</html>
