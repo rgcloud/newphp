3f5c39e26f1378db1321888cd0630c41c4920e23
rgcloud <37324173+rgcloud@users.noreply.github.com>
2019-03-18 22:12:04 +0530

    t7

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 <title> ..:::: Welcome To SGcloud ::::.. </title>
   
-  <center><img src="P1.jpg"  width="213" height="92"></center>
+  
 <center> <h1> Welcome To Our Openshift DevOps(CI/CD) Project</h1> </center>
 
 <?php
@@ -12,5 +12,8 @@ echo "<center><h2>How are you getting on :-) V2.0  </h2></center>";
   
 
 ?>
+  
+  
+  <center><img src="img_chania.jpg" alt="Flowers in Chania" width="460" height="345"></center>
 </body>
 </html>
