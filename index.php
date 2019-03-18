bf571a24f663b991e665b0e1dd006415753e737d
rgcloud <37324173+rgcloud@users.noreply.github.com>
2019-03-18 20:22:11 +0530

    t1

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ echo "<center><h2>How are you getting on :-) V2.0  </h2></center>";
   
 
 ?>
-
+<IMG SRC="P1.jpg" ALT="some text" WIDTH=300 HEIGHT=200>
+  //<IMG SRC="P2.jpg" ALT="some text" WIDTH=300 HEIGHT=200>
 </body>
 </html>
